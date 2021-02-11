df08b66ac14037a469dc630dc76449c012b242b9
Ian Hancock <northroadian@gmail.com>
2021-02-11 07:09:38 -0600

    adding side bar

--- src/SideBarRow.js
+++ src/SideBarRow.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Avatar } from '@material-ui/core';
 import "./SidebarRow.css";
-function SideBarRow() {
+
+function SideBarRow({src,title,Icon}) {
     return (
         <div className = "sidebarRow">
-            <p>I am a roooow</p>
+            {src && <Avatar src ={src} />}
+            {Icon && <Icon />}
+            
+            <h4>{title} </h4>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import "./Sidebar.css";
 import SideBarRow from './SideBarRow'
 function Sidebar() {
     return (
         <div className="sidebar" >
-           <SideBarRow />
+          <SidebarRow src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fiso.500px.com%2Fthe-top-20-nature-photos-on-500px-so-far-this-year%2F&psig=AOvVaw3hIbgj76lrYtrpxvWR7vZ1&ust=1613135194504000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMizo4Xz4e4CFQAAAAAdAAAAABAD' title="Ian Hancock" />
+          
+          
+           <SideBarRow title= 'Pages'/>
+           <SideBarRow title= 'Friends'/>
+           <SideBarRow title= 'Pages'/>
+           <SideBarRow title= 'Pages'/>
+           <SideBarRow title= 'Pages'/>
         </div>
     )
 }
