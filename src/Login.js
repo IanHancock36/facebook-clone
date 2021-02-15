e6d9b981693380ecb3a0d566217e03528ffab8cd
Ian Hancock <northroadian@gmail.com>
2021-02-15 14:29:20 -0600

    adding firebase and google authentication

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Login.css";
 import Button from '@material-ui/core/Button';
-
+import {auth, provider} from "./firebase"
 function Login() {
   const signIn = ()=> {
-    // sign in stuff 
+    auth.signInWithPopup(provider)
+    .then(result => {
+      console.log(result);
+    })
+    .catch((error) => alert(error.message));
   }
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//const firebaseConfig = {
- // apiKey: "AIzaSyDdtY2HlZTyjm2kWlykMBPOWkikDyX1e-A",
-//  authDomain: "facebook-clone-7b522.firebaseapp.com",
- // projectId: "facebook-clone-7b522",
- // storageBucket: "facebook-clone-7b522.appspot.com",
-  //messagingSenderId: "501780403929",
- // appId: "1:501780403929:web:f6c2b25ca866b1b63b10c8",
-//  measurementId: "G-S11VPD0B7Y",
-//};
+ import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyDdtY2HlZTyjm2kWlykMBPOWkikDyX1e-A",
+ authDomain: "facebook-clone-7b522.firebaseapp.com",
+  projectId: "facebook-clone-7b522",
+ storageBucket: "facebook-clone-7b522.appspot.com",
+  messagingSenderId: "501780403929",
+  appId: "1:501780403929:web:f6c2b25ca866b1b63b10c8",
+ measurementId: "G-S11VPD0B7Y",
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider };
+export default db; 
